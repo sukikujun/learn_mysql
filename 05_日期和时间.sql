2409bcc90d39e6680990596117c00f8a85b8d094
jyun <kikusujun@gmail.com>
2024-02-16 19:56:44 +0900

    date_format time_format

--- 05_日期和时间.sql
+++ 05_日期和时间.sql
@@ -3,3 +3,6 @@ show databases;
 -- #Datetime 
 alter table stu add birthday datetime default null;
 
+-- date_format()
+select sname, date_format(birthday, '%Y-%m-%d %h:%i:%s') from stu;
+select sname, time_format(birthday, '%h:%i:%s') from stu;
