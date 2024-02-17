c26e089048a811f00009206ebfc64693f59f1596
jyun <kikusujun@gmail.com>
2024-02-17 18:53:33 +0900

    SUM() AVG()

--- 06_统计汇总.sql
+++ 06_统计汇总.sql
@@ -14,3 +14,11 @@ select sname, birthday, month(birthday) as m from stu order by m desc;
 select sname, birthday, mid(birthday, 6, 2) as m from stu where birthday is not null order by m;
 
 select * from stu where year(birthday) > 1990 order by birthday asc limit 1;
+
+-- SUM() AVG()
+select SUM(click) from article;
+select AVG(click) from article;
+
+select * from article where click < (select AVG(click) from article);
+
+select round(avg(timestampdiff(year,birthday,now()))) as age from stu;
