cfb21e38345429a9a4256de8f9b940d360444d43
kikusujun <kikusujun@gmail.com>
2024-02-20 11:54:18 +0800

    begin / start transaction

--- 08_事务处理.sql
+++ 08_事务处理.sql
@@ -2,3 +2,14 @@
 show engines;
 
 alter table stu engine = 'InnoDB';
+
+-- begin transaction
+begin;
+insert into stu(sname, class_id, sex) values('刘洁', 2, 2);
+insert into class(cname) values ('研究生');
+commit;
+
+start transaction;
+insert into stu(sname, class_id, sex) values('李海', 2, 2);
+rollback;
+
