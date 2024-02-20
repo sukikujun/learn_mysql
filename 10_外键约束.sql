cde5f6346694e51cbdd4c80bb114416f4b89dd1a
jyun <kikusujun@gmail.com>
2024-02-20 22:25:16 +0900

    添加/删除 外键约束

--- 10_外键约束.sql
+++ 10_外键约束.sql
@@ -9,3 +9,8 @@ create table stu2 (
     references class(id)
     on delete cascade
 ) engine=InnoDB default charset=utf8mb4;
+
+-- 添加/删除 外键约束
+alter table stu add constraint stu_class foreign key (class_id) references class(id) on delete cascade;
+
+alter table stu drop foreign key stu_class;
